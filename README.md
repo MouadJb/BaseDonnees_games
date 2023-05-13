d6c5b01c476ad3f28e2a8ce08ea0c59aaedbbfc6
unknown <dirassa0@gmail.com>
2023-05-13 17:26:45 +0100

    version1

---
+++ README.md
@@ -0,0 +1,2 @@
+This project was created using a public API from "RAWG Video Games" and Angular for frontend.
+Ce projet a été créé en utilisant une API publique de "RAWG Video Games" et Angular pour le frontend.
---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: '9b1bb42d98e1423989cd02f65641ca3e',
+      }
+    });
+    return next.handle(req);
+  }
+}
